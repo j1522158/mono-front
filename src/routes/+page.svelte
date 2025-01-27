be5f5ba9f655e11870b417b658cd9ac72170c1e5
j1522158 <j1522158vu@gmail.com>
2025-01-27 22:11:19 +0900

    Effects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,19 @@
 <script>
-	let numbers = $state([1, 2, 3, 4]);
-	let total = $derived(numbers.reduce((t, n) => t + n, 0));
+	let elapsed = $state(0);
+	let interval = $state(1000);
 
-	function addNumber() {
-		numbers.push(numbers.length + 1);
-	}
+	$effect(() => {
+		const id = setInterval(() => {
+			elapsed += 1;
+		}, interval);
 
-	$inspect(numbers).with(console.trace);
+		return () => {
+			clearInterval(id);
+		};
+	});
 </script>
 
-<p>{numbers.join(' + ')} = {total}</p>
+<button onclick={() => interval /= 2}>speed up</button>
+<button onclick={() => interval *= 2}>slow down</button>
 
-<button onclick={addNumber}>
-	Add a number
-</button>
+<p>elapsed: {elapsed}</p>
