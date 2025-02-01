f2e3a073e2835eb4a08326e788f15baef5e67d57
j1522158 <j1522158vu@gmail.com>
2025-02-01 19:57:11 +0900

    Inline handlers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
 	let m = $state({ x: 0, y: 0 });
-
-	function onpointermove(event) {
-		m.x = event.clientX;
-		m.y = event.clientY;
-	}
 </script>
 
-<div {onpointermove}>
+<div
+	onpointermove={(event) => {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}}
+>
 	The pointer is at {Math.round(m.x)} x {Math.round(m.y)}
 </div>
 
